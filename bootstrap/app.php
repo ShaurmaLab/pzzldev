a068bb596360d7869239dc74add4502e49ec8549
Paul Ivanov <paul@shaurmalab.com>
2024-12-13 11:40:14 +0300

    [B1] C11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,31 +12,8 @@
 */
 
 $app = new Illuminate\Foundation\Application(
-    realpath(__DIR__.'/../')
+    realpath(__DIR__ . '/../')
 );
-
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
-*/
-
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
 
 $app->register(Irazasyed\LaravelGAMP\LaravelGAMPServiceProvider::class);
 
